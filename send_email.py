7cb5e3241d0220b8dd06f11ed823eb0380b2073a
kazem3d <kazem3d@gmail.com>
2022-12-23 19:03:02 +0330

    feat:add title in searching

--- send_email.py
+++ send_email.py
@@ -21,23 +21,29 @@ engine = create_engine("sqlite:///sqlite.db", echo=True, future=True)
 
 EMAIL_USERNAME = os.environ.get('EMAIL_USERNAME')
 EMAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
-contacts = ['rezaghanaty@gmail.com','kazem3d@gmail.com',]
-
-
-
+contacts = ['rezaghanaty@gmail.com', 'kazem3d@gmail.com',]
 
 
 class DesireContent:
-    def __init__(self,organization_name) :
-        self._organization_name = organization_name
+    '''
+        Get content that you define in initialization of object from database
+        and return it as text context
+    '''
+
+    def __init__(self, organization_name='', title=''):
+        self.__organization_name = organization_name
+        self.__title = title
         self.__context = ''
 
     def __search_database(self):
         with Session(engine) as session:
-            stmt = select(Needs).where(Needs.is_sent == False,Needs.org_name.contains(self._organization_name))
-            for row in  session.execute(stmt).scalars().all():
-                self.__context +=f"{row.board_name} شماره نیاز: {row.number} -- عنوان: {row.title} -- {row.org_name} -- شهر: {row.city_name} \n \n"
-            session.commit()  
+            stmt = select(Needs).where(Needs.is_sent == False,
+                                       Needs.org_name.contains(
+                                           self.__organization_name),
+                                       Needs.title.contains(self.__title))
+            for row in session.execute(stmt).scalars().all():
+                self.__context += f"{row.board_name} شماره نیاز: {row.number} -- عنوان: {row.title} -- {row.org_name} -- شهر: {row.city_name} \n \n"
+            session.commit()
 
     def get_context(self):
         self.__search_database()
@@ -45,14 +51,17 @@ class DesireContent:
 
     def tag_is_sent(self):
         with Session(engine) as session:
-            stmt = select(Needs).where(Needs.is_sent == False,Needs.org_name.contains(self._organization_name))
-            for row in  session.execute(stmt).scalars().all():
+            stmt = select(Needs).where(Needs.is_sent == False,
+                                       Needs.org_name.contains(
+                                           self.__organization_name),
+                                       Needs.title.contains(self.__title))
+            for row in session.execute(stmt).scalars().all():
                 row.is_sent = True
-            session.commit()  
+            session.commit()
 
 
 class EmailContext:
-    def __init__(self,sender,receivers,subject,context):
+    def __init__(self, sender, receivers, subject, context):
         self.__email_message = EmailMessage()
         self.__email_message['From'] = sender
         self.__email_message['To'] = receivers
@@ -61,7 +70,7 @@ class EmailContext:
 
     def send_email(self):
         # print('*******',len(self.__email_message.get_content()))
-        if len(self.__email_message.get_content()) > 2: #check don't send empty massage
+        if len(self.__email_message.get_content()) > 2:  # check don't send empty massage
             with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
                 smtp.login(EMAIL_USERNAME, EMAIL_PASSWORD)
                 smtp.send_message(self.__email_message)
@@ -69,10 +78,15 @@ class EmailContext:
 
 if __name__ == '__main__':
 
-    rahdari_context =  DesireContent('راهداری')
+    rahdari_context = DesireContent(organization_name='راهداری')
     context = rahdari_context.get_context()
-    email_obj = EmailContext(EMAIL_USERNAME,contacts,'اعلام نیاز های راهداری کل کشور',context)
+    email_obj = EmailContext(EMAIL_USERNAME, contacts,
+                             'اعلام نیاز های راهداری کل کشور', context)
     email_obj.send_email()
     rahdari_context.tag_is_sent()
 
-   
+    software_context = DesireContent(title='نرم افزار')
+    email_obj = EmailContext(EMAIL_USERNAME, 'kazem3d@gmail.com',
+                             'اعلام نیاز های نرم افزاری', software_context.get_context())
+    email_obj.send_email()
+    software_context.tag_is_sent()
